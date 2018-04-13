ad341c181c456e04c6bba148a7a599e15e5477d7
borismueller <boris.mueller@iet-gibb.ch>
2018-04-13 19:22:30 +0200

    ups

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Dev(models.Model):
     user_name = models.CharField(max_length=25)
     name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length)
+    last_name = models.CharField(max_length=25)
     email = models.EmailField()
     password = models.CharField(max_length=25)
     projects = models.PositiveIntegerField() # hmmm
     status = models.PositiveSmallIntegerField()
-    profile_pic = models.CharField(max_lenght=30) # path 
+    profile_pic = models.CharField(max_length=30) # path 
