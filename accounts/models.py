811d28181cacf8180bdf479c57e44a9b41656039
borismueller <boris.mueller@iet-gibb.ch>
2018-04-13 19:12:29 +0200

    started db stuff

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Dev eveveveve
+class Dev(models.Model):
+    user_name = models.CharField(max_length=25)
+    name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length)
+    email = models.EmailField()
+    password = models.CharField(max_length=25)
+ 
